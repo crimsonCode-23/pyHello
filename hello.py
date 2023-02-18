ed3ffddb578a41d2a52dbc34c5ca1d2d4abc8838
riley924 <108380624+riley924@users.noreply.github.com>
2023-02-18 14:23:01 -0800

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,3 +1,6 @@
 msg = "pleaseWork"
 print(msg)
 # practice repo for the hackathon. Feel free to pull, push, whatever! 
+print("hi")
+TOKEN = 'MTA3NjYxNTcwNTY2NDk0NjIwNg.GRJixX.6TlklQxbyMguHfWSGLCW30AQd2uAJZoUsP1h5I'
+print("hi")
